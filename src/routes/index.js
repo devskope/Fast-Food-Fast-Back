c1df7357c47cb50d04004243807f438564000d01
ope-oguntoye <ope.oguntoye@protonmail.com>
2018-09-07 18:24:08 +0100

    feat(#160343263): add user registration and login functionality

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
-/* eslint-disable */
-const ROOT_URL = `/api/v1`;
+import userRouter from './users/index';
+
+export const ROOT_URL = `/api/v1`;
 
 const router = app => {
+  app.use(`${ROOT_URL}/users`, userRouter);
 };
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import morgan from 'morgan';
+import { user } from './datastores/userData';
 import r from './routes/index';
 
 const app = express();
@@ -9,17 +10,24 @@ const { env } = process;
 if (!env.NODE_ENV) {
   dotenv.config();
 }
+
 const port = env.PORT;
+
 if (env.NODE_ENV === `production`) {
   app.use(morgan('combined'));
 } else {
   app.use(morgan('dev'));
 }
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  req.user = user;
+  res.locals.user = req.user;
+  next();
+});
 r(app);
-app.listen(port);
 
-const ok = name => `${name}'s Okayy!`;
+app.listen(port, () => console.log(`running on ${port}`));
 
-export default ok;
+export default app;
