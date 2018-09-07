b6d7afdaf77ec5dd16a9512ce733b97635e4f76e
ope-oguntoye <ope.oguntoye@protonmail.com>
2018-09-07 23:37:14 +0100

    reorganize test suite

--- test/test.js
+++ test/test.js
@@ -1,15 +1,87 @@
-import dotenv from 'dotenv';
-import { expect } from 'chai';
-import ok from '../src/server';
+import chai from 'chai';
+import chaiHttp from 'chai-http';
 
-dotenv.config();
-const { env } = process;
+import server from '../src/server';
+import { users } from '../src/datastores/userData';
+import orders from '../src/datastores/orderData';
 
-if (env.NODE_ENV !== 'test') env.NODE_ENV = 'test';
+const { expect } = chai;
+chai.use(chaiHttp);
 
-describe('I test okay', () => {
-  const name = 'fast-food-fast-back';
-  it('should test okay', () => {
-    expect(ok(name)).to.equal(`${name}'s Okayy!`);
+const ROOT_URL = `/api/v1`;
+
+const orderToMake = {
+  name: 'nugg',
+  qty: '4',
+  category: 'pizza'
+};
+
+describe('app flow', () => {
+  describe('unregistered', () => {
+    it('annon user cannot make orders', () => {
+      chai
+        .request(server)
+        .post(`${ROOT_URL}/orders`)
+        .send(orderToMake)
+        .end((err, res) => {
+          expect(res.status).eq(401);
+          expect(res.body.success).eq(false);
+          expect(res.body.message).eq(
+            `you must be logged in to access the requested resource`
+          );
+        });
+    });
+  });
+
+  /* 
+
+
+  */
+
+  describe('register -> login -> order', () => {
+    const newUser = {
+      username: `martin-Skope`,
+      password: `blabla`,
+      email: `nyet@pillow.me`
+    };
+
+    it('Should register new user', () => {
+      chai
+        .request(server)
+        .post(`${ROOT_URL}/users/register`)
+        .send(newUser)
+        .end((err, res) => {
+          expect(res.status).eq(201);
+        });
+    });
+
+    it('Should save user to datastore', () => {
+      expect(Boolean(users.findByUsername(newUser.username))).eq(true);
+    });
+
+    it('Registered user can login', () => {
+      chai
+        .request(server)
+        .post(`${ROOT_URL}/users/login`)
+        .send(newUser)
+        .end((err, res) => {
+          expect(res.status).eq(200);
+          expect(res.body.message).eq(
+            `successful login as ${newUser.username}`
+          );
+        });
+    });
+
+    it('logged in user can create an order', () => {
+      chai
+        .request(server)
+        .post(`${ROOT_URL}/orders`)
+        .send(orderToMake)
+        .end((err, res) => {
+          expect(res.status).eq(201);
+          expect(res.body.message).eq(`Order created successfully`);
+          expect(typeof res.body.data).eq('object');
+        });
+    });
   });
 });
