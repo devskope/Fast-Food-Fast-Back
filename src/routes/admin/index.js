08252b3d33aeed6b9ff0bebf7cbb76ee817587f3
ope oguntoye <ope.oguntoye@protonmail.com>
2018-09-12 17:52:26 +0100

    feat(#160446774): Add logout route

--- src/routes/admin/index.js
+++ src/routes/admin/index.js
@@ -4,5 +4,6 @@ import controller from './adminController';
 const router = express.Router();
 
 router.post('/login', controller.loginAdmin);
+router.post('/logout', controller.logoutAdmin);
 
 export default router;
