541567ed65bb20fd75043f8a88bab9cb91411c02
ope oguntoye <ope.oguntoye@protonmail.com>
2018-09-12 17:31:46 +0100

    feat(#160446774): Add logout route

--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -7,5 +7,6 @@ router.post('/register', controllers.createUser);
 router.post('/login', controllers.userLogin);
 router.get('/register', controllers.redirectAuthPages);
 router.get('/login', controllers.redirectAuthPages);
+router.get('/logout', controllers.userLogout);
 
 export default router;
