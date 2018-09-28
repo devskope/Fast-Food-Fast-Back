e76796cabc44fe27900c4e1f5d21fff95b2802fa
ope oguntoye <ope.oguntoye@protonmail.com>
2018-09-28 12:02:55 +0100

    ch(#160829827): remove showdown

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,24 +1,11 @@
-import fs from 'fs';
-import path from 'path';
-import { Converter } from 'showdown';
-
 import userRouter from './users/index';
 import orderRouter from './orders/index';
 
 export const ROOT_URL = `/api/v1`;
 
-const converter = new Converter();
-converter.setFlavor('github');
-
 const router = app => {
   app.use(`${ROOT_URL}/users`, userRouter);
   app.use(`${ROOT_URL}/orders`, orderRouter);
-  app.use(`${ROOT_URL}`, (req, res) => {
-    fs.readFile(path.join(__dirname, '../../docs/doc.md'), (err, data) => {
-      if (err) throw err;
-      res.status(200).send(converter.makeHtml(data.toString()));
-    });
-  });
 };
 
 export default router;
