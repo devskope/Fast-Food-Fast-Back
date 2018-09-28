ef8cb53cb471fd5b97db206a1692bf635d414617
ope oguntoye <ope.oguntoye@protonmail.com>
2018-09-28 09:09:23 +0100

    ch(#160829827): validate request params

--- src/routes/users/userController.js
+++ src/routes/users/userController.js
@@ -1,8 +1,6 @@
 import bcrypt from 'bcryptjs';
 import User from '../../models/users';
 import { user, users } from '../../datastores/userData';
-
-import checkRequired from '../../helpers/auth_field_helpers';
 import { ROOT_URL } from '../index';
 
 const userExists = username => Boolean(users.findByUsername(username));
@@ -10,6 +8,20 @@ const findUser = username => users.findByUsername(username);
 
 const createUser = (req, res) => {
   const errors = [];
+  ['username', 'password'].map(field => {
+    if (req.body[field] === undefined) {
+      errors.push({
+        category: 'validation',
+        message: `misssing required ${field} field`
+      });
+    }
+  });
+  if (!/(.*)@(.*)\.(.*)/.test(req.body.email)) {
+    errors.push({
+      category: 'validation',
+      message: 'invalid email format'
+    });
+  }
 
   if (!req.user.anonymous) {
     res.status(307).json({
@@ -17,111 +29,106 @@ const createUser = (req, res) => {
       location: `${req.headers.host + ROOT_URL}/menu`,
       message: `already logged in as ${req.user.details.username}`
     });
+  } else if (errors.length > 0) {
+    res.status(400).json({
+      success: false,
+      errors
+    });
+  } else if (userExists(req.body.username)) {
+    res.status(409).json({
+      success: false,
+      message: `user '${req.body.username}' already exists`
+    });
   } else {
-    checkRequired(req, errors);
+    const userToCreate = {
+      username: req.body.username,
+      password: req.body.password,
+      email: req.body.email || ''
+    };
 
-    if (errors.length > 0) {
-      res.status(400).json({
-        success: false,
-        errors
-      });
-    } else if (userExists(req.body.username)) {
-      res.status(409).json({
-        success: false,
-        message: `user '${req.body.username}' already exists`
-      });
-    } else {
-      const userToCreate = {
-        username: req.body.username,
-        password: req.body.password,
-        email: req.body.email || ''
-      };
+    const newUser = new User(userToCreate);
 
-      const newUser = new User(userToCreate);
+    bcrypt.genSalt(10, (gErr, salt) => {
+      if (gErr) {
+        res.status(500).json({
+          success: true,
+          message: `An unexpected error occured, please try again`
+        });
+      } else {
+        bcrypt.hash(newUser.password, salt, (hErr, hash) => {
+          if (hErr) {
+            res.status(500).json({
+              success: true,
+              message: `An unexpected error occured, please try again`
+            });
+          } else {
+            newUser.password = hash;
+            newUser.save();
+          }
+        });
+      }
+    });
 
-      bcrypt.genSalt(10, (gErr, salt) => {
-        if (gErr) {
-          res.status(500).json({
-            success: true,
-            message: `An unexpected error occured, please try again`
-          });
-        } else {
-          bcrypt.hash(newUser.password, salt, (hErr, hash) => {
-            if (hErr) {
-              res.status(500).json({
-                success: true,
-                message: `An unexpected error occured, please try again`
-              });
-            } else {
-              newUser.password = hash;
-              newUser.save();
-            }
-          });
-        }
-      });
-
-      res.status(201).json({
-        success: true,
-        message: `user ${newUser.username} registered successfully`
-      });
-    }
+    res.status(201).json({
+      success: true,
+      message: `user ${newUser.username} registered successfully`
+    });
   }
 };
 
-/* 
-
-
-*/
-
 const userLogin = (req, res) => {
   const errors = [];
+  ['username', 'password'].map(field => {
+    if (req.body[field] === undefined) {
+      errors.push({
+        category: 'validation',
+        message: `misssing required ${field} field`
+      });
+    }
+  });
   if (!req.user.anonymous) {
     res.status(307).json({
       success: true,
       location: `${req.headers.host + ROOT_URL}/menu`,
       message: `already logged in as ${req.user.details.username}`
     });
+  } else if (errors.length > 0) {
+    res.status(400).json({
+      success: false,
+      errors
+    });
+  } else if (!userExists(req.body.username)) {
+    res.status(401).json({
+      success: false,
+      message: `user ${req.body.username} not registered`
+    });
   } else {
-    checkRequired(req, errors);
+    const match = findUser(req.body.username);
 
-    if (errors.length > 0) {
-      res.status(400).json({
-        success: false,
-        errors
-      });
-    } else if (!userExists(req.body.username)) {
-      res.status(401).json({
-        success: false,
-        message: `user ${req.body.username} not registered`
-      });
-    } else {
-      const match = findUser(req.body.username);
+    if (bcrypt.compare(req.body.password, match.password)) {
+      user.details = { ...match };
+      user.anonymous = false;
+      delete user.details.password;
 
-      if (bcrypt.compare(req.body.password, match.password)) {
-        user.details = { ...match };
-        user.anonymous = false;
-        delete user.details.password;
-
-        if (match.isAdmin) {
-          res.status(200).json({
-            success: true,
-            message: `successful login as admin user`,
-            user
-          });
-        } else {
-          res.status(200).json({
-            success: true,
-            message: `successful login as ${req.user.details.username}`,
-            user
-          });
-        }
-      } else {
-        res.status(400).json({
+      if (match.isAdmin) {
+        res.status(200).json({
           success: true,
-          message: `password incorrect`,
+          message: `successful login as admin user`,
+          user
+        });
+      } else {
+        res.status(200).json({
+          success: true,
+          message: `successful login as ${req.user.details.username}`,
           user
         });
       }
+    } else {
+      res.status(400).json({
+        success: true,
+        message: `password incorrect`,
+        user
+      });
     }
   }
 };
