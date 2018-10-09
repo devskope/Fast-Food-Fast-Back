21bc93a996c8f9f4b4503c4d6bce0468523b8b18
ope oguntoye <ope.oguntoye@protonmail.com>
2018-10-09 20:13:18 +0100

    ft(#161038831): implement RBAC for routes with jwt middlewares

--- src/routes/orders/index.js
+++ src/routes/orders/index.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import controllers from './orderController';
 import validateParams from '../../middlewares/orders/validateParams';
+import validUser from '../../middlewares/auth/validUser';
+import onlyAdmin from '../../middlewares/auth/onlyAdmin';
 
 const router = express.Router();
 
-router.get('/:id', controllers.getSingleOrder);
-router.get('/', controllers.getOrders);
+router.get('/:id', validUser, controllers.getSingleOrder);
+router.get('/', validUser, controllers.getOrders);
 
-router.post('/', validateParams, controllers.createOrder);
+router.post('/', validUser, validateParams, controllers.createOrder);
 
-router.put('/:id', controllers.updateOrder);
+router.put('/:id', validUser, onlyAdmin, controllers.updateOrder);
 
 export default router;
